b4eee609e390d225868984d9e72f55c63e237779
Paul Wahtel <iwahtelt@gmail.com>
2023-09-03 16:07:26 +0300

    add animation for Menu component, polish Menu component

--- src/Core/Chat/components/Menu/Menu.jsx
+++ src/Core/Chat/components/Menu/Menu.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import styled from "@emotion/native";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Animated } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import OutsidePressHandler from "react-native-outside-press";
 import { PlusIcon } from "./PlusIcon";
+import { MenuItem } from "./MenuItem";
 
 const MenuContainer = styled.TouchableOpacity`
   height: 40px;
@@ -14,92 +15,81 @@ const MenuContainer = styled.TouchableOpacity`
 }
 `;
 
-const Menu = styled.View`
+const Menu = styled(Animated.View)`
   width: 194px;
   min-height: 88px;
-  // background-color: red;
   background-color: #191919;
   position: absolute;
   border-radius: 12px;
   top: -100px;
 `;
 
-const ItemContainer = styled.TouchableOpacity`
-  flex: 1;
-  flex-direction: column;
-  padding: 11px 16px 11px 16px;
-  justify-content: center; // This will vertically center the immediate child.
-`;
-
-const TextWrapper = styled.View`
-  flex: 1;
-  justify-content: center; // This will vertically center the Text inside this View.
-  display: flex;
-`;
-
-const ItemWrapper = styled.View`
-  flex: 1;
-  justify-content: space-between; // This will vertically center the Text inside this View.
-  display: flex;
-  flex-direction: row;
-`;
-
-const Item = styled.Text`
-  font-family: "SF-Pro-Text-Regular";
-  font-size: 15px;
-  line-height: 22px;
-  // text-align: left;
-  color: #ffffff;
-`;
-
 const Divider = styled.View`
   height: 1px;
   background-color: #545458;
-  position: absolute;
+  // position: absolute;
   width: 100%;
-  left: 0;
-  right: 0;
-  top: 50%;
+  // left: 0;
+  // right: 0;
+  // top: 50%;
 `;
 
 export const MenuComponent = () => {
   const [visible, setVisible] = useState(false);
+  const [menuAnimation] = useState(new Animated.Value(0));
+  const anchroColor = visible ? "#0C7BEB" : "#FFFFFF";
+
+  const openMenu = () => {
+    Animated.timing(menuAnimation, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const closeMenu = () => {
+    Animated.timing(menuAnimation, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: true,
+    }).start(() => setVisible(false)); // Hide the menu entirely after animation
+  };
 
   const toggleMenu = () => {
-    setVisible(!visible);
+    if (visible) {
+      closeMenu();
+    } else {
+      setVisible(true); // Show the menu for the animation
+      openMenu();
+    }
   };
-  
-  const openMenu = () => {
-    setVisible(true);
-  };
-
-  const closeMenu = () => setVisible(false);
 
   const renderMenu = () => (
-    <Menu>
+    <Menu
+      style={{
+        opacity: menuAnimation,
+        transform: [
+          {
+            translateY: menuAnimation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [30, 0], // adjust these numbers for desired animation effect
+            }),
+          },
+        ],
+      }}
+    >
+      <MenuItem text="Item 1" Icon={PlusIcon} />
       <Divider />
-      <ItemContainer onPress={e => e.preventDefault} activeOpacity={1}>
-        <TextWrapper>
-          <ItemWrapper>
-            <Item>Item 1</Item>
-            <PlusIcon />
-          </ItemWrapper>
-        </TextWrapper>
-        <TextWrapper>
-          <Item>Item 2</Item>
-        </TextWrapper>
-      </ItemContainer>
+      <MenuItem text="Item 2" Icon={PlusIcon} />
     </Menu>
   );
 
   return (
-    <OutsidePressHandler
-      onOutsidePress={closeMenu}
-    >
+    <OutsidePressHandler onOutsidePress={closeMenu}>
       <SafeAreaView style={{ zIndex: 1 }}>
         <MenuContainer onPress={toggleMenu} activeOpacity={1}>
           {visible && renderMenu()}
-          <Icon name="dots-vertical" size={30} color={"white"} />
+          <Icon name="dots-vertical" size={30} color={anchroColor} />
         </MenuContainer>
       </SafeAreaView>
     </OutsidePressHandler>
---
+++ src/Core/Chat/components/Menu/MenuItem.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "@emotion/native";
+
+const Container = styled.TouchableOpacity`
+  flex: 1;
+  display: flex;
+  padding: 0 16px;
+`;
+
+const ItemWrapper = styled.View`
+  flex: 1;
+  justify-content: space-between; // This will vertically center the Text inside this View.
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Item = styled.Text`
+  font-family: "SF-Pro-Text-Regular";
+  font-size: 15px;
+  line-height: 22px;
+  color: #ffffff;
+`;
+
+export const MenuItem = (props) => {
+  const { text, Icon } = props;
+
+  return (
+    <Container onPress={(e) => e.preventDefault}>
+      <ItemWrapper>
+        <Item>{text}</Item>
+        <Icon />
+      </ItemWrapper>
+    </Container>
+  );
+};
