f3983e200053f8cfbf83a843e80cc5422a33c11e
Bohdan Mykhayliv <bohdan.mykhayliv@bohdanmykhayliv.local>
2023-09-14 22:46:13 +0300

    Add .prettierrc.json file

--- src/Core/Tasks/TasksComponent.jsx
+++ src/Core/Tasks/TasksComponent.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "@emotion/native";
-import {ScrollView} from "react-native";
-import {TasksHint, Categories} from "./components";
+import React from 'react';
+import styled from '@emotion/native';
+import { ScrollView } from 'react-native';
+import { TasksHint, Categories } from './components';
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -15,40 +15,40 @@ const Text = styled.Text`
 const Tasks = [
   {
     id: 1,
-    title: "Post",
-    description: "Compose engaging instagram post",
-    icon: "instagram",
-    color: "#8959C733",
+    title: 'Post',
+    description: 'Compose engaging instagram post',
+    icon: 'instagram',
+    color: '#8959C733',
     prompt:
-      "As a content strategist specializing in Instagram engagement, draft a post on [topic]. This post targets [business description] and should resonate with [target audience]. The main aim of this post is [post objective]. Please create an Instagram post that aligns with the specified objectives and audience preferences.",
+      'As a content strategist specializing in Instagram engagement, draft a post on [topic]. This post targets [business description] and should resonate with [target audience]. The main aim of this post is [post objective]. Please create an Instagram post that aligns with the specified objectives and audience preferences.'
   },
   {
     id: 2,
-    title: "Post",
-    description: "Compose engaging instagram post",
-    icon: "facebook",
-    color: "#8959C733",
+    title: 'Post',
+    description: 'Compose engaging instagram post',
+    icon: 'facebook',
+    color: '#8959C733',
     prompt:
-      "As a content strategist specializing in Instagram engagement, draft a post on [topic]. This post targets [business description] and should resonate with [target audience]. The main aim of this post is [post objective]. Please create an Instagram post that aligns with the specified objectives and audience preferences.",
+      'As a content strategist specializing in Instagram engagement, draft a post on [topic]. This post targets [business description] and should resonate with [target audience]. The main aim of this post is [post objective]. Please create an Instagram post that aligns with the specified objectives and audience preferences.'
   },
   {
     id: 3,
-    title: "Post",
-    description: "Compose engaging instagram post",
-    icon: "x",
-    color: "#8959C733",
+    title: 'Post',
+    description: 'Compose engaging instagram post',
+    icon: 'x',
+    color: '#8959C733',
     prompt:
-      "As a content strategist specializing in Instagram engagement, draft a post on [topic]. This post targets [business description] and should resonate with [target audience]. The main aim of this post is [post objective]. Please create an Instagram post that aligns with the specified objectives and audience preferences.",
+      'As a content strategist specializing in Instagram engagement, draft a post on [topic]. This post targets [business description] and should resonate with [target audience]. The main aim of this post is [post objective]. Please create an Instagram post that aligns with the specified objectives and audience preferences.'
   },
   {
     id: 4,
-    title: "Post",
-    description: "Compose engaging instagram post",
-    icon: "tiktok",
-    color: "#8959C733",
+    title: 'Post',
+    description: 'Compose engaging instagram post',
+    icon: 'tiktok',
+    color: '#8959C733',
     prompt:
-      "As a content strategist specializing in Instagram engagement, draft a post on [topic]. This post targets [business description] and should resonate with [target audience]. The main aim of this post is [post objective]. Please create an Instagram post that aligns with the specified objectives and audience preferences.",
-  },
+      'As a content strategist specializing in Instagram engagement, draft a post on [topic]. This post targets [business description] and should resonate with [target audience]. The main aim of this post is [post objective]. Please create an Instagram post that aligns with the specified objectives and audience preferences.'
+  }
 ];
 
 export const TasksComponent = () => {
@@ -56,9 +56,9 @@ export const TasksComponent = () => {
     <Container>
       <ScrollView>
         <TasksHint icon='🥳' title='Welcome to Promts!'
-                   description='You can choose a topic to use the chat more conveniently'/>
-        <Categories title="Recent" tasks={Tasks}/>
-        <Categories title="Suggested" tasks={Tasks}/>
+                   description='You can choose a topic to use the chat more conveniently' />
+        <Categories title='Recent' tasks={Tasks} />
+        <Categories title='Suggested' tasks={Tasks} />
       </ScrollView>
     </Container>
   );
